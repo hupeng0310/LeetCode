8535fa3782b0b006c30bcd2c35f3664ee9a020e3
hupeng0310 <hupgwuty@163.com>
2019-09-15 16:57:37 +0800

    错误的版本

--- src/com/Leetcode/twosum/TwoSum.java
+++ src/com/Leetcode/twosum/TwoSum.java
@@ -14,6 +14,7 @@ public class TwoSum {
         return new int[]{};
     }
     public static void main(String args[]){
+        
         System.out.println(Arrays.toString(towsum(new int[]{1,2,3,4,5},4)));
     }
 }
