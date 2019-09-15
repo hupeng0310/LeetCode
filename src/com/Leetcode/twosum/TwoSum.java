15b450056676335b05a2a495a81486a0e7286bb2
hupeng0310 <hupgwuty@163.com>
2019-09-15 14:03:11 +0800

    创建Leetcode算法题仓库

---
+++ src/com/Leetcode/twosum/TwoSum.java
@@ -0,0 +1,10 @@
+package com.Leetcode.twosum;
+
+public class TwoSum {
+    public static int[] towsum(int[] nums,int num){
+        return nums;
+    }
+    public static void main(String args[]){
+
+    }
+}
