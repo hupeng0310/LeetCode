e4ab760e1af35e346b0230d2db4c5f7e798bf129
hupeng0310 <hupgwuty@163.com>
2019-09-15 17:01:56 +0800

    错误的版本

--- src/com/Leetcode/twosum/TwoSum.java
+++ src/com/Leetcode/twosum/TwoSum.java
@@ -3,18 +3,39 @@ package com.Leetcode.twosum;
 import java.util.Arrays;
 
 public class TwoSum {
+    //beginIndex表示二分后的数组[0]在原数组中的位置
+    public static int[] towsum(int num,int []nums,int target,int beginIndex){
+        if(nums.length == 0){
+            return new int[]{};
+        }else {
+            int half = nums.length/2;
+            if (num + nums[half] == target) {
+                return new int[]{num, beginIndex + half};
+            } else if (num + nums[half] > target) {
+                towsum(num, Arrays.copyOfRange(nums, 0, half), target, beginIndex);
+            } else {
+                towsum(num, Arrays.copyOfRange(nums, half + 1, nums.length), target, beginIndex + half + 1);
+            }
+        }
+        return new int[]{};
+    }
     public static int[] towsum(int[] nums,int target){
-        for(int i = 0;i < nums.length;i++){
-            for(int j = i+1;j < nums.length;j++){
-                if(nums[i] + nums [j] == target){
-                    return new int[] {i,j};
-                }
+        Arrays.sort(nums);
+        int[] result;
+        for(int i = 0;i<nums.length;i++){
+            if(i == nums.length-1){
+                return new int[]{};
+            }
+            result = towsum(nums[i],Arrays.copyOfRange(nums,i+1,nums.length),target,i+1);
+            if(result.length==0){
+                continue;
+            }else {
+                return new int[] {i,result[1]};
             }
         }
         return new int[]{};
     }
     public static void main(String args[]){
-        
-        System.out.println(Arrays.toString(towsum(new int[]{1,2,3,4,5},4)));
+        System.out.println(Arrays.toString(towsum(new int[]{1,2,3,4,5,6},9)));
     }
 }
