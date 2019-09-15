43e258ae2ad38d4f94dc2e280267d510ee8188f0
hupeng0310 <hupgwuty@163.com>
2019-09-15 14:34:42 +0800

    两数之和问题的暴力破解方法

--- src/com/Leetcode/twosum/TwoSum.java
+++ src/com/Leetcode/twosum/TwoSum.java
@@ -1,10 +1,19 @@
 package com.Leetcode.twosum;
 
+import java.util.Arrays;
+
 public class TwoSum {
     public static int[] towsum(int[] nums,int target){
-        return nums;
+        for(int i = 0;i < nums.length;i++){
+            for(int j = i+1;j < nums.length;j++){
+                if(nums[i] + nums [j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[]{};
     }
     public static void main(String args[]){
-
+        System.out.println(Arrays.toString(towsum(new int[]{1,2,3,4,5},4)));
     }
 }
