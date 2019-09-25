3642e39f2129261a010c67e08ae147e806946fa3
hupeng0310 <hupgwuty@163.com>
2019-09-25 20:00:38 +0800

    提交了整数反转问题的代码

---
+++ src/com/Leetcode/reverse/Reverse.java
@@ -0,0 +1,27 @@
+package com.Leetcode.reverse;
+
+public class Reverse {
+    private String reverse(String str){
+        StringBuffer buffer = new StringBuffer();
+        char []array = str.toCharArray();
+        for(int i = array.length-1;i >= 0;i--){
+            buffer.append(array[i]);
+        }
+        return buffer.toString();
+    }
+    public int reverse(int x){
+        long num = x;
+        if(num>=0){
+            long newNum = Long.parseLong(reverse(Long.toString(num)));
+            return newNum > Integer.MAX_VALUE?0:(int)newNum;
+        }
+        else {
+            long result = -Long.parseLong(reverse(Long.toString(num).substring(1)));
+            return result<Integer.MIN_VALUE?0:(int)result;
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Reverse().reverse(-4536));
+    }
+}
