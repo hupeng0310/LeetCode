a31b9b5581804ffdc6e5c18d212b160b35d0b51a
hupeng0310 <hupgwuty@163.com>
2019-09-26 16:51:53 +0800

    提交了有效括号(easy)的答案(使用stack)

---
+++ src/com/Leetcode/easy/Valid/Valid.java
@@ -0,0 +1,50 @@
+package com.Leetcode.easy.Valid;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Valid {
+    private char getTargetChar(char c){
+        switch (c){
+            case '(' :
+                return ')';
+            case ')' :
+                return '(';
+            case '[' :
+                return ']';
+            case ']' :
+                return '[';
+            case '{' :
+                return '}';
+            case '}' :
+                return '{';
+            default:
+                return ' ';
+        }
+    }
+
+    public boolean isValid(String s){
+        if(s.length()%2!=0){
+            return false;
+        }else if(s.length() == 0){
+            return true;
+        }else {
+            Stack<Integer> target = new Stack<Integer>();
+            for(int i = 0;i < s.length();i++){
+                if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                    target.push(i);
+                }else {
+                    if(target.empty()){
+                        return false;
+                    }else if(s.charAt(i) == getTargetChar(s.charAt(target.peek()))) {
+                        target.pop();
+                    }
+                }
+            }
+            return target.empty();
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(new Valid().isValid("{}{{}}"));
+    }
+}
