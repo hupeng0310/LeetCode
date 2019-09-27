87baf711b5735ede2dc9454623246e6f472d1da1
hupeng0310 <hupgwuty@163.com>
2019-09-27 18:18:17 +0800

    提交了搜索插入位置问题的答案（二分查找)

--- src/com/algorithm/easy/searchInsert/SearchInsert.java
+++ src/com/algorithm/easy/searchInsert/SearchInsert.java
@@ -1,18 +1,27 @@
 package com.algorithm.easy.searchInsert;
 
+import java.util.Arrays;
+
 class SearchInsert{
+	private int indexOf = -0;
 	public int searchInsert(int[] nums,int target){
 		if(nums.length == 0){
-			return -1;
-		}
-		for(int i = 0;i < nums.length;i++){
-			if(nums[i] == target){
-				return i;
+			return 0;
+		}else if(nums.length == 1){
+			return target <= nums[0]?indexOf:indexOf+1;
+		}else {
+			int halfLength = nums.length / 2;
+			if(target == nums[halfLength] || (target > nums[halfLength -1] && target < nums[halfLength])){
+				return indexOf +halfLength;
+			}else if(target < nums[halfLength]){
+				return searchInsert(Arrays.copyOfRange(nums,0,halfLength-1),target);
+			}else {
+				indexOf += halfLength;
+				return searchInsert(Arrays.copyOfRange(nums,halfLength,nums.length),target);
 			}
 		}
-		return -1;
 	}
 	public static void main(String[] args){
-		System.out.println(new SearchInsert().searchInsert(new int[]{1,2,3,4,5},3));
+		System.out.println(new SearchInsert().searchInsert(new int[]{1,3},-1));
 	}
 }
